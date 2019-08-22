e928041bbc5ca52cd781b78965487030443e811a
Tommy Chen <tommy351@gmail.com>
2019-08-22 14:54:25 +0800

    ci: Generate versioned deployment

---
+++ hack/generate-versioned-deployment.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+PROJECT_ROOT="$(cd "$(dirname "$0")/.." ; pwd)"
+DEPLOYMENT_DIR=$PROJECT_ROOT/deployment
+TMP_DEPLOYMENT_DIR=$(mktemp -d)
+IMAGE_TAG=${1:-latest}
+OUTPUT_PATH=$PROJECT_ROOT/dist/pullup-deployment.yml
+
+cleanup() {
+  rm -rf ${TMP_DEPLOYMENT_DIR}
+}
+trap "cleanup" EXIT SIGINT
+
+cp -a ${DEPLOYMENT_DIR}/* ${TMP_DEPLOYMENT_DIR}
+
+echo "
+images:
+  - name: tommy351/pullup-controller
+    newTag: ${IMAGE_TAG}
+  - name: tommy351/pullup-webhook
+    newTag: ${IMAGE_TAG}
+" >> ${TMP_DEPLOYMENT_DIR}/kustomization.yml
+
+mkdir -p $(dirname $OUTPUT_PATH)
+$PROJECT_ROOT/assets/bin/kubectl kustomize $TMP_DEPLOYMENT_DIR > $OUTPUT_PATH
