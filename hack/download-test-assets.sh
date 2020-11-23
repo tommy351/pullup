4bc3a8281d8ebeee28769cc20992df794ebf217d
Tommy Chen <tommy351@gmail.com>
2020-11-23 18:18:06 +0800

    test: Update test assets

--- hack/download-test-assets.sh
+++ hack/download-test-assets.sh
@@ -24,22 +24,18 @@ etcd_dest="${dest_dir}/etcd"
 kubectl_dest="${dest_dir}/kubectl"
 kube_apiserver_dest="${dest_dir}/kube-apiserver"
 kind_dest="${dest_dir}/kind"
-kustomize_dest="${dest_dir}/kustomize"
 
 echo "About to download a couple of binaries. This might take a while..."
 
 mkdir -p "$dest_dir"
 
-k8s_version=1.16.4
+k8s_version=1.19.2
 curl $quiet -L "https://go.kubebuilder.io/test-tools/${k8s_version}/${os_lowercase}/amd64" | tar --strip-components=2 -xz -C "$dest_dir" kubebuilder/bin
 
-kind_version=v0.8.1
+kind_version=v0.9.0
 curl $quiet -L "https://github.com/kubernetes-sigs/kind/releases/download/${kind_version}/kind-${os_lowercase}-amd64" --output "$kind_dest"
 
-kustomize_version=v3.1.0
-curl $quiet -L "https://github.com/kubernetes-sigs/kustomize/releases/download/${kustomize_version}/kustomize_$(echo -n $kustomize_version | sed 's/^v//')_${os_lowercase}_amd64" --output "$kustomize_dest"
-
-chmod +x "$etcd_dest" "$kubectl_dest" "$kube_apiserver_dest" "$kind_dest" "$kustomize_dest"
+chmod +x "$etcd_dest" "$kubectl_dest" "$kube_apiserver_dest" "$kind_dest"
 
 echo    "# destination:"
 echo    "#   ${dest_dir}"
@@ -48,4 +44,3 @@ echo -n "#   etcd:            "; "$etcd_dest" --version | head -n 1
 echo -n "#   kube-apiserver:  "; "$kube_apiserver_dest" --version
 echo -n "#   kubectl:         "; "$kubectl_dest" version --client --short
 echo -n "#   kind:            "; "$kind_dest" --version
-echo -n "#   kustomize:       "; "$kustomize_dest" version
